0399d7da2806260515bc730b9c065c27527abbb7
“brianstevo” <brianaranha76@gmail.com>
2021-03-17 22:03:06 +0530

    footer and header setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import {Container} from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen'
 const App=() =>{
   return (
-    <div className="App">
-     hi
-    </div>
+    <>
+    <Header/>
+    <main>
+      <Container>
+        <HomeScreen/>    
+      </Container>
+    </main>
+    <Footer/>
+  </>
   );
 }
 
